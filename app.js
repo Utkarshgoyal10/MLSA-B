9ce7a8bd76aabbc6f6e571a6f9995006f3e5442e
Utkarsh Goyal <124898881+Utkarshgoyal10@users.noreply.github.com>
2024-04-29 19:52:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ import cookieParser from "cookie-parser"
 
 const app = express()
 
-app.use(cors({ credentials: true , origin: 'http://localhost:5173' }))
+app.use(cors({
+    origin: '*',
+  }));
+  app.use(cors());
+app.use(cors(""));
+
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
